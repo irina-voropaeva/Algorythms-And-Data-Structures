3560c691de1471647f1f65e169fce67ae34b57a1
Lunmijo <sparrow.dreamer.dog@gmail.com>
2019-12-11 21:09:22 +0200

    lab4 started

---
+++ laboratory_works/lab4/bitvectorset.py
@@ -0,0 +1,21 @@
+class BitVectorSet:
+    def __init__(self, size):
+        self.array = []
+        for n in range(size):
+            self.array[n] = False
+
+    def __len__(self):
+        return len(self.array)
+
+    def __contains__(self, el: int):
+        if len(self.array) < (el - 1):
+            return False
+        return self.array[el] == True
+
+    def insert(self, el: int):
+        if el not in self:
+            self.array[el] = True
+
+    def remove(self, el: int):
+        if el in self:
+            self.array[el] = False
